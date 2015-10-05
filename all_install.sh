c46701181b59837960650a09bb01612914eefe13
skrzepto <skrzepto@hawk.iit.edu>
2015-10-05 07:36:39 -0500

    fixing up zshrc and fedora install

---
+++ all_install.sh
@@ -0,0 +1,4 @@
+chsh -s /bin/zsh
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
---
+++ fedora_install.sh
@@ -0,0 +1,8 @@
+sudo dnf install zsh i3 arandr atom redshift firefox midori cmus clementine vim vlc keepass mplayer transmission libreoffice network-manager-applet git pcmanfm thunar gummi mupdf lxterminal rxvt-unicode htop nitrogen filezilla powerline tmux tmux-powerline vim-plugin-powerline
+
+# missing veracrypt youtube-dl claws-mail
+
+
+
+# spideroak install for fedora systems
+cd ~/Downloads ; wget https://spideroak.com/getbuild?platform=fedora&arch=x86_64 ; sudo rpm -i getbuild?platform=fedora&arch=x86_64
