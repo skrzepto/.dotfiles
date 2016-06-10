60a3692ffaea033f694ab0c918cba8b1c24a92d9
skrzepto <skrzepto@gmail.com>
2016-06-10 14:20:47 -0500

    fixed atom package script

--- atom/atom_pkg_manager.sh
+++ atom/atom_pkg_manager.sh
@@ -1,8 +1,32 @@
+#!/bin/bash
+# Bash Menu Script Example
 
-# this will save the atom packages
-apm list --installed --bare > package-list.txt
 
+save_packages () {
+  # this will save the atom packages
+  apm list --installed --bare > package-list.txt
+}
+install_packages () {
+  # this will install from the text
+  apm install --packages-file package-list.txt
+}
 
-# this will install from the text
-apm install --packages-file package-list.txt
-
+PS3='Please enter your choice: '
+options=("Save Atom packages" "Install Atom packages" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Save Atom packages")
+            save_packages
+            break
+            ;;
+        "Install Atom packages")
+            install_packages
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
