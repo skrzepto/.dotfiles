e88640c408ff77a3565235cbdf912a4056bc5a62
skrzepto <skrzepto@gmail.com>
2016-06-10 14:00:50 -0500

    made it less usesr interactive

--- fedora_install.sh
+++ fedora_install.sh
@@ -5,8 +5,10 @@ sudo dnf install zsh i3 i3status arandr redshift glances lxterminal htop
 
 # file explorer / archival / cloud
 sudo dnf install pcmanfm thunar file-roller
-sudo dnf copr enable decathorpe/syncthing && sudo dnf install syncthing
-sudo  dnf copr enable pkerling/seafile  && sudo dnf install seafile-client-qt  # to run `$ seafile-applet` 
+echo "copr enable decathorpe/syncthing"
+sudo dnf copr enable -y decathorpe/syncthing && sudo dnf install syncthing
+echo "copr enable pkerling/seafile"
+sudo  dnf copr enable -y pkerling/seafile  && sudo dnf install seafile-client-qt  # to run `$ seafile-applet` 
 # insync checkout their website. to start run `$ insync start`
 # duplicati
 
@@ -16,16 +18,21 @@ sudo  dnf copr enable pkerling/seafile  && sudo dnf install seafile-client-qt  #
 
 # dev tools
 sudo dnf install filezilla tmux git sqlitebrowser
-sudo dnf copr enable phracek/PyCharm && sudo dnf install pycharm-community
-sudo dnf copr enable helber/atom && sudo dnf install atom
-sudo dnf copr enable dperson/neovim && sudo dnf install neovim
+echo "copr enable phracek/PyCharm"
+sudo dnf copr enable -y phracek/PyCharm && sudo dnf install pycharm-community
+echo "copr enable helber/atom"
+sudo dnf copr enable -y helber/atom && sudo dnf install atom
+echo "copr enable dperson/neovim"
+sudo dnf copr enable -y dperson/neovim && sudo dnf install neovim
 
 # communication
 sudo dnf install evolution mumble hexchat
 
 # media
 sudo dnf install youtube-dl  clementine vlc
-sudo dnf copr enable region51/gnome-mpv && sudo dnf install gnome-mpv
+echo "about to enable rpm-fusion"
+sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install mpv
 
 # editors / pdf 
 sudo dnf install libreoffice gummi mupdf evince
@@ -38,6 +45,7 @@ sudo dnf install keepass keepassx
 
 # misc
 sudo dnf install transmission shutter
+
 
 
 
