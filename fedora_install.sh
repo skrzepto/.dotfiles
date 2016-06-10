16ff4ca22ad812b9e8837ff087ca1c7b75ea0aaa
skrzepto <skrzepto@gmail.com>
2016-06-10 13:50:20 -0500

    cleaned up the fedora install script so i could migrate main pc to f24 soon

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
-sudo dnf install zsh i3 i3status arandr redshift firefox midori vim keepass transmission libreoffice network-manager-applet git pcmanfm thunar gummi mupdf lxterminal htop nitrogen filezilla tmux evince shutter file-roller weechat fuse-encfs
 
-#sudo dnf install gnome-boxes virt-manager 
+# desktop / shell
+sudo dnf install zsh i3 i3status arandr redshift glances lxterminal htop
 
-#vim-plugin-powerline
+# file explorer / archival / cloud
+sudo dnf install pcmanfm thunar file-roller
+sudo dnf copr enable decathorpe/syncthing && sudo dnf install syncthing
+sudo  dnf copr enable pkerling/seafile  && sudo dnf install seafile-client-qt  # to run `$ seafile-applet` 
+# insync checkout their website. to start run `$ insync start`
+# duplicati
 
-# missing veracrypt youtube-dl claws-mail
+# file encryption
+# veracrypt
+# cryptomator
 
-
-rpm --import "http://rpmfusion.org/keys?action=AttachFile&do=get&target=RPM-GPG-KEY-rpmfusion-free-fedora-23" "http://rpmfusion.org/keys?action=AttachFile&do=get&target=RPM-GPG-KEY-rpmfusion-nonfree-fedora-23"
-sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-23.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-23.noarch.rpm
-
-sudo dnf install mpv
-
-
-# spideroak install for fedora systems
-cd ~/Downloads
-wget https://spideroak.com/getbuild?platform=fedora&arch=x86_64
-mv getbuild?platform=fedora&arch=x86_64 getbuild?platform=fedora&arch=x86_64.rpm 
-sudo dnf install getbuild?platform=fedora&arch=x86_64.rpm
-sudo dnf install libfreetype.so.6 libXext.so.6 libXrender.so.1 libfontconfig.so.1
-
-
+# dev tools
+sudo dnf install filezilla tmux git sqlitebrowser
+sudo dnf copr enable phracek/PyCharm && sudo dnf install pycharm-community
 sudo dnf copr enable helber/atom && sudo dnf install atom
+sudo dnf copr enable dperson/neovim && sudo dnf install neovim
 
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# communication
+sudo dnf install evolution mumble hexchat
+
+# media
+sudo dnf install youtube-dl  clementine vlc
+sudo dnf copr enable region51/gnome-mpv && sudo dnf install gnome-mpv
+
+# editors / pdf 
+sudo dnf install libreoffice gummi mupdf evince
+
+# internet browsers
+sudo dnf install firefox midori google-chrome lynx
+
+# password managers
+sudo dnf install keepass keepassx 
+
+# misc
+sudo dnf install transmission shutter
 
 
 
 
 
-# dnf copr enable surkum/gencfsm && dnf install gencfsm
