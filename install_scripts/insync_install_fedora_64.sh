565929998e857f3a14d22e0ecc6f6504c53b5c6f
skrzepto <shims506@gmail.com>
2016-06-14 17:43:12 -0500

    moving install scripts to their own folder, added notes on xrandr for multi screen config

---
+++ install_scripts/insync_install_fedora_64.sh
@@ -0,0 +1,3 @@
+cd ~/Downloads
+wget http://s.insynchq.com/builds/insync-1.3.8.36087-fc21.x86_64.rpm
+sudo dnf install ./insync-1.3.8.36087-fc21.x86_64.rpm
---
+++ notes/xrandr.md
@@ -0,0 +1,4 @@
+```
+xrandr --output DVI-D-1 --auto --rotate right --right-of DVI-I-1
+xrandr --output DVI-D-1 --pos 1920x-300
+```
