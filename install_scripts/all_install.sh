fb806c4cd91e746356287293a0360969f7c01312
skrzepto <shims506@gmail.com>
2016-10-28 22:53:42 -0500

    cleaning up script to check if files exist or not

--- install_scripts/all_install.sh
+++ install_scripts/all_install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# currently optimized for a fedora system
 
 # set defautl shell to zsh and install oh-my-zsh
-chsh -s /bin/zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+command -v zsh >/dev/null 2>&1 || { echo "I require zsh but it's not installed.  Aborting." >&2; exit 1; }
+if [ $(echo $SHELL)  != "/bin/zsh" ] ; then
+    chsh -s /bin/zsh
+fi
+
+if [ ! -d ~/.oh-my-zsh ] ; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 
 # if that doesn't work i will need to manually edit it using `vipw`
 # yourusername:x:1000:1000:optional full name here:/home/yourusername:/bin/zsh
@@ -18,11 +24,9 @@ mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 git config --global core.excludesfile $dir/.gitignore_global
 
 # soft link i3 config
-mkdir $XDG_CONFIG_HOME/i3
-mv $XDG_CONFIG_HOME/i3/config $XDG_CONFIG_HOME/i3/config.bak
-ln -s $dir/i3/config $XDG_CONFIG_HOME/i3/
-mv $XDG_CONFIG_HOME/i3/i3status.conf $XDG_CONFIG_HOME/i3/i3status.conf.bak
-ln -s $dir/i3/i3status.conf $XDG_CONFIG_HOME/i3/
+if [ ! -d $XDG_CONFIG_HOME/i3/ ] ; then
+    ln -s $dir/i3/ $XDG_CONFIG_HOME/
+fi
 
 # soft link neovim config
 # ln -s $dir/vim/.vim $XDG_CONFIG_HOME/nvim
@@ -33,7 +37,10 @@ mv $HOME/.vimrc $HOME/.vimrc.bak
 ln -s $dir/vim/.vimrc $HOME/
 
 # pull vundle for nvim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -f ~/.vim/bundle/Vundle.vim] 
+    then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 nvim -c BundleInstall -c q -c q
 
 # soft link zshrc
@@ -41,14 +48,14 @@ mv $HOME/.zshrc $HOME/.zshrc.bak
 ln -s $dir/.zshrc $HOME/
 
 # soft link atom package.cson
-ln -s $dir/atom/packages.cson $HOME/.atom/packages.cson
-apm install 'package-sync'
-apm install --packages-file $dir/atom/package-list.txt
+# ln -s $dir/atom/packages.cson $HOME/.atom/packages.cson
+# apm install 'package-sync'
+# apm install --packages-file $dir/atom/package-list.txt
 
 # soft link taskrc
-mkdir $HOME/.task
-mv $HOME/.taskrc $HOME/.taskrc.bak
-ln -s $dir/.taskrc $HOME/
+# mkdir $HOME/.task
+# mv $HOME/.taskrc $HOME/.taskrc.bak
+# ln -s $dir/.taskrc $HOME/
 
 # GIT configuration
 git config --global user.name "skrzepto"
