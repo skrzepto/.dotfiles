c5ae253959da6e4c14636f2bcf4436660c51c061
Szymon Mucha <shims506@gmail.com>
2022-04-30 20:05:27 -0500

    more path migration to make symlinking easier

--- i3/README.md
+++
@@ -1,4 +0,0 @@
-To run the automated i3wm configuration provisioning run
-```
-bash ./install_i3_config.sh
-```
--- scripts/multi-monitor-config.sh
+++
@@ -1,3 +0,0 @@
-xrandr --output DVI-D-1 --auto --rotate right --right-of DVI-I-1
-xrandr --output DVI-D-1 --pos 1920x-300
-
