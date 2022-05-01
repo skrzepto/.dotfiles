a668c6bacfd35147b9fd798e0717706cf1738c92
Szymon Mucha <shims506@gmail.com>
2022-04-30 21:25:39 -0500

    minimum apps to get a ubuntu dev env setup wit i3wm

---
+++ debian-apps.sh
@@ -0,0 +1,11 @@
+UBUNTU="\
+git tig i3 i3lock rofi  redshift network-manager htop pavucontrol xclip thunar nautilus file-roller \
+tmux neovim shutter xarchiver glances arandr scrot xbacklight redshift-gtk exa ripgrep rsync ranger \
+fzf fd-find zsh \
+"
+
+# copyq golang syncthing lxterminal 
+
+sudo apt install $UBUNTU
+chsh -s /bin/zsh
+
