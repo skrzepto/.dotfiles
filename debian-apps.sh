977e385ef32a0edf731dfde7c067ef2bb9066312
Szymon Mucha <shims506@gmail.com>
2022-04-30 23:03:22 -0500

    use different backlight utility

--- debian-apps.sh
+++ debian-apps.sh
@@ -1,11 +1,11 @@
 UBUNTU="\
 git tig i3 i3lock rofi  redshift network-manager htop pavucontrol xclip thunar nautilus file-roller \
 tmux neovim shutter xarchiver glances arandr scrot xbacklight redshift-gtk exa ripgrep rsync ranger \
-fzf fd-find zsh \
+fzf fd-find zsh light\
 "
 
 # copyq golang syncthing lxterminal 
 
 sudo apt install $UBUNTU
 chsh -s /bin/zsh
-
+sudo chmod u+s $(which light)
