13ed09fd3126ce1297410ad099eede5a6ccd4acb
Szymon Mucha <shims506@gmail.com>
2022-04-30 22:21:59 -0500

    More i3wm cleanup

---
+++ install.sh
@@ -0,0 +1,12 @@
+DOTFILES=$HOME/.dotfiles
+STOW_FOLDERS="i3,nvim,zsh"
+#!/usr/bin/env zsh
+# I am using zsh instead of bash.  I was having some troubles using bash with
+# arrays.  Didn't want to investigate, so I just did zsh
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    stow -D $folder
+    stow $folder
+done
+popd
