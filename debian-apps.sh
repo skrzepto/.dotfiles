8ddc4c1e3da34a213562fe33b3170b5c70cf398b
Szymon Mucha <shims506@gmail.com>
2022-05-02 17:05:46 -0500

    additional apps to install

--- debian-apps.sh
+++ debian-apps.sh
@@ -1,11 +1,22 @@
 UBUNTU="\
 git tig i3 i3lock rofi  redshift network-manager htop pavucontrol xclip thunar nautilus file-roller \
 tmux neovim shutter xarchiver glances arandr scrot xbacklight redshift-gtk exa ripgrep rsync ranger \
-fzf fd-find zsh light alacritty stow blueman \
+fzf fd-find zsh light alacritty stow blueman software-properties-common parallel \
 "
 
 # copyq golang syncthing lxterminal 
-
+echo "Install ubuntu apps"
 sudo apt install $UBUNTU
+
+echo "Change default shell to zsh"
 chsh -s /bin/zsh
+
+echo "Make light cmd which manages backlight executable by no-sudo users"
 sudo chmod u+s $(which light)
+
+echo "Install safe eyes applicationw wish helps rest eyes from monitor"
+sudo add-apt-repository ppa:slgobinath/safeeyes && sudo apt update && sudo apt instlal safeeyes
+
+
+
+
